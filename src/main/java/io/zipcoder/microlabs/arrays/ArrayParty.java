829961a0d79e56ba83b14a92bc99ca25c8bd9fe7
vidyachandasekhar <vidyahari.vs@Gmail.com>
2017-05-10 13:48:58 -0400

    >array list >last element in array >last but 1 element

--- src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
+++ src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
@@ -3,24 +3,51 @@ package io.zipcoder.microlabs.arrays;
 
 public class ArrayParty {
 
-    public String printArray(String[] inputArray){
-        return "";
+
+    public String printArray(String[] inputArray) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("*** Output ***");
+        for (String food : inputArray) {
+            stringBuilder.append("\n" + food);
+
+        }
+        return stringBuilder.toString();
+
     }
 
-    public String lastElement(String[] inputArray){
-        return "";
+
+
+    public String lastElement(String[] inputArray) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("*** Output ***\n");
+        stringBuilder.append(inputArray[inputArray.length - 1]);
+
+        return stringBuilder.toString();
     }
 
-    //TODO Define the method lastButOne
-
-    //TODO Define the method reverse
-
-    //TODO Define the method isPalindrome
-
-    //TODO Define the method compress
-
-    //TODO Define the method pack
 
 
+    public String lastButOne(String[] inputArray) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("*** Output ***\n");
+        stringBuilder.append(inputArray[inputArray.length - 2]);
+
+        return stringBuilder.toString();
+    }
 
 }
+
+//TODO Define the method lastButOne
+
+//TODO Define the method reverse
+
+//TODO Define the method isPalindrome
+
+//TODO Define the method compress
+
+//TODO Define the method pack
+
+    
+
+
--- src/test/java/io/zipcoder/microlabs/arrays/ArrayPartyTest.java
+++ src/test/java/io/zipcoder/microlabs/arrays/ArrayPartyTest.java
@@ -28,11 +28,34 @@ public class ArrayPartyTest {
     @Test
     public void lastElementTest(){
         //: Given
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+        ArrayParty arrayParty = new ArrayParty();
+        String expected = "*** Output ***\n" +
+                "Mushrooms";
 
         //: When
+        String actual = arrayParty.lastElement(breakfast);
 
         //: Then
+        Assert.assertEquals("The two string outputs are equal", expected, actual);
     }
+
+    @Test
+    public void lastButOneTest(){
+        //: Given
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+        ArrayParty arrayParty = new ArrayParty();
+        String expected = "*** Output ***\n" +
+                "Tomatoes";
+
+        //: When
+        String actual = arrayParty.lastButOne(breakfast);
+
+        //: Then
+        Assert.assertEquals("The two string outputs are equal", expected, actual);
+    }
+
+
 
     //TODO Define the method lastButOneTest
 
